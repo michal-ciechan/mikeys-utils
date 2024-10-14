5af875b2f4600ce064195251e9c51f5051860cec
Mike Ciechan <lndcobra@hotmail.com>
2024-10-14 09:27:00 +0100

    Improving DateTime Parsing Tests

--- MikeysUtils3.UnitTests/DateTimeParsingSnapshotTests.cs
+++ MikeysUtils3.UnitTests/DateTimeParsingSnapshotTests.cs
@@ -8,7 +8,7 @@ public class DateTimeParsingSnapshotTests
     record Result(DateTimeType DateTimeType, string ParsedDateTime, DateTimeKind Kind);
     
     [Test]
-    public async Task SnapshotTest()
+    public async Task UniversaleSortableNoT1()
     {
         var input = "2024-10-23 21:39:40.1234567";
 
@@ -18,7 +18,119 @@ public class DateTimeParsingSnapshotTests
             results,
             """
             - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
-                ParsedDateTime: 2024-10-23T21:39:40.1234567
+              ParsedDateTime: 2024-10-23T21:39:40.1234567
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTSingleMillisecond()
+    {
+        var input = "2024-10-23 21:39:40.1";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:39:40.1000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTNoMilliseconds()
+    {
+        var input = "2024-10-23 21:39:40";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:39:40.0000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTHasFullstop()
+    {
+        var input = "2024-10-23 21:39:40.";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:39:40.0000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTNoSecondsTrailingColon()
+    {
+        var input = "2024-10-23 21:39:";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:39:00.0000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTNoSeconds()
+    {
+        var input = "2024-10-23 21:39";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:39:00.0000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTNoMinutesTrailingColon()
+    {
+        var input = "2024-10-23 21:";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:00:00.0000000
+            """
+        );
+    }
+    
+    [Test]
+    public async Task UniversaleSortableNoTNoMinutes()
+    {
+        var input = "2024-10-23 21";
+
+        var results = Parse(input);
+
+        InlineSnapshot.Validate(
+            results,
+            """
+            - DateTimeType: DateTime_UniversalSortable_yyyy_MM_dd_HH_mm_ss
+              ParsedDateTime: 2024-10-23T21:00:00.0000000
             """
         );
     }
